4fc015d510b36393f5ce0b1f864c53250fe999dd
QElis <thomas.moor@fau.de>
2025-06-22 23:29:52 +0200

    priorities

--- plugins/priorities/tutorial/pev_4_versbd.cpp
+++ plugins/priorities/tutorial/pev_4_versbd.cpp
@@ -15,19 +15,19 @@ int main(int argc, char* argv[]) {
     FairGeneratorVector gvoi;
     EventPriorities prios;
     
+    // read priorities
+    prios.Read("data/pev_4_prios.alph");
+
     // read models
+    gvoi.Clear();
     gvoi.Append(FairGenerator("data/pev_4_sbd_m12.gen"));
     gvoi.Append(FairGenerator("data/pev_4_sbd_p2.gen"));
     gvoi.Append(FairGenerator("data/pev_4_sbd_take_l2.gen"));
-    //gvoi.Append(FairGenerator("data/pev_4_one_wpon_cb.gen"));
+    gvoi.Append(FairGenerator("data/pev_4_one_wpon_cb.gen"));
     gvoi.Append(FairGenerator("data/pev_4_sbd_send2.gen"));
     gvoi.Append(FairGenerator("data/pev_4_g_rbpm_coupl.gen"));
     gvoi.Append(FairGenerator("data/pev_4_sbd_m22.gen")); 
     
-    // read priorities
-    prios.Read("data/pev_4_prios.alph");
-
-
     // do compositional verification
     std::cout<<"starting compositional verification" << std::endl;
     auto start = std::clock();
@@ -36,12 +36,32 @@ int main(int argc, char* argv[]) {
     if(isnc)
       std::cout<<"p-nonconflicting test passed"<<std::endl;
     else
-      std::cout<<"p-nonconflicting test failed"<<std::endl;
+      std::cout<<"p-nonconflicting test failed [testcase ERROR]"<<std::endl;
 
     // record test case
     FAUDES_TEST_DUMP("prio comp ver",isnc);
 
-    // done
+    // read models (without "wpon")
+    gvoi.Clear();
+    gvoi.Append(FairGenerator("data/pev_4_sbd_m12.gen"));
+    gvoi.Append(FairGenerator("data/pev_4_sbd_p2.gen"));
+    gvoi.Append(FairGenerator("data/pev_4_sbd_take_l2.gen"));
+    gvoi.Append(FairGenerator("data/pev_4_sbd_send2.gen"));
+    gvoi.Append(FairGenerator("data/pev_4_g_rbpm_coupl.gen"));
+    gvoi.Append(FairGenerator("data/pev_4_sbd_m22.gen")); 
+
+    // do compositional verification
+    std::cout<<"starting compositional verification" << std::endl;
+    start = std::clock();
+    bool isnnc = IsPFNonblocking(gvoi,prios);
+    std::cout<<"duration in seconds: "<<ToStringFloat((std::clock()-start)/(double) CLOCKS_PER_SEC)<<std::endl;
+    if(isnnc)
+      std::cout<<"p-nonconflicting test passed [testcase ERROR]"<<std::endl;
+    else
+      std::cout<<"p-nonconflicting test failed"<<std::endl;
+
+    // record test case
+    FAUDES_TEST_DUMP("prio comp ver",isnnc);    // done
     return 0;
 
 }
