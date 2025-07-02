7539cb25dc95ab0bf493e00e1b784306b0b281ab
QElis <thomas.moor@fau.de>
2025-07-02 11:47:53 +0200

    images5

--- plugins/iodevice/tutorial/create_images.sh
+++ plugins/iodevice/tutorial/create_images.sh
@@ -37,16 +37,6 @@ done;
 rm tmp_elevator_plant_full.*
 rm tmp_elevator_spec_full.*
 
-#DOCU_GENS="elevator_plant_cabin elevator_plant_door elevator_plant_lbarrier elevator_spec_core elevator_super_core elevator_spec_cabmot elevator_spec_opcmd elevator_spec_cabmots elevator_spec_opcmds elevator_spec_scheds elevator_spec_drsaf1 elevator_spec_drsaf2 elevator_spec_drmot1 elevator_super_full decdemo_planta decdemo_plantb decdemo_contra decdemo_contrb decdemo_cloop decdemo_hcloop"
-
-## loop all .gen files
-#for BASE in $DOCU_GENS ; do
-#  echo ============= processing $BASE
-#  $DOTWRITE tmp_$BASE.gen
-#  $DOTEXEC -Tsvg -Gbgcolor=transparent -Gsize=10,10 tmp_$BASE.dot -o tmp_$BASE.svg
-#  $CONVERT -background none tmp_$BASE.svg tmp_$BASE.png
-#done;
-
 # pass on to common image processing
 . $LIBFAUDES/tools/imgproc/imgproc.sh
 
