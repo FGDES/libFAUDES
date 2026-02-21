e297a50adc43ab8dd2c10c3f451180a42b120249
QElis <thomas.moor@fau.de>
2026-02-21 23:11:49 +0100

    wheels

--- plugins/pybindings/src/setup.py
+++ plugins/pybindings/src/setup.py
@@ -7,16 +7,24 @@ from datetime import datetime
 # figure source files
 srcfiles = [str(cppfile) for cppfile in Path("src/").glob("**/*.cpp")]
 
-# figure build
+# figure platform
 platform = sysconfig.get_platform()
 pyver = sysconfig.get_python_version()
 buildstr = platform + "-for-python-" + pyver
+
+# figure time stamp
 nowstr = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
+
+# figure relevant system  libraries
+syslibs=[]
+if "win" in platform:
+  syslibs=["winmm", "wsock32"]
 
 ext = Extension(
   name="_faudes",
   sources=srcfiles,
   include_dirs=["src"],
+  libraries=syslibs,
   define_macros=[
     ("FAUDES_BUILD_DSO","1"),
     ("FAUDES_MUTE_RTIAUTOLOAD","1"),
